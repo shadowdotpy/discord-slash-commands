f7ec31fc44c57d61ac0aacadf61a3aba7d1bd070
notSh4d0w <74737342+shadowdotpy@users.noreply.github.com>
2022-05-19 21:04:41 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,23 @@
+import discord # Normal Discord Environment
+from discord.ext import commands # Normal Commands (@bot.command)
+from discord_slash import SlashCommand # Slash Commands (@slash.slash)
+
+bot = discord.Client(prefix=".", intents=discord.Intents.all())
+slash = SlashCommand(bot, sync_commands=True)
+
+@bot.event
+async def on_ready():
+    print("Bot Ready")
+
+
+@bot.command()
+async def test(ctx):
+    await ctx.send("Das ist ein Test Chat Command")
+
+
+@slash.slash(name="test", description="Ein Test Command")
+async def test(ctx):
+    await ctx.send("Das ist ein Test Slash Command")
+
+
+bot.run("DEIN_TOKEN")
